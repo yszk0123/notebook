b64d749b33389fd84df38b4e609f0ef1c0185a5d
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-11-25 18:22:29 +0900

    feat(editor-type): add MenuItem type

--- src/modules/editor/editor-type.ts
+++ src/modules/editor/editor-type.ts
@@ -1,4 +1,19 @@
+import { Schema } from 'prosemirror-model';
+import { EditorState, Transaction } from 'prosemirror-state';
+
 export interface NodeAsJSON {
   type: string;
   content: NodeAsJSON[];
+}
+
+// tslint:disable-next-line:no-any
+type Command<S extends Schema = any> = (
+  state: EditorState<S>,
+  dispatch?: (tr: Transaction<S>) => void,
+) => boolean;
+
+export interface MenuItem {
+  command: Command;
+  shortTitle: string;
+  longTitle: string;
 }
