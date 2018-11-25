74caa6c11dce7ae663994dde22f4fac414337a2c
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-11-25 18:21:32 +0900

    fix(editor): add ThemeProvider to example

--- src/modules/editor/example.tsx
+++ src/modules/editor/example.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ResetStyle } from '../../components/ResetStyle';
+import { ThemeProvider } from '../../styled-components';
+import { defaultTheme } from '../../theme/theme';
 import { printError } from '../../utils/printError';
 import { Editor } from './components/Editor';
 import './registerProseMirror';
@@ -13,10 +15,12 @@ export async function render() {
   const mountPoint = document.getElementById('root');
 
   ReactDOM.render(
-    <>
-      <Editor onChange={noop} content={null} />
-      <ResetStyle />
-    </>,
+    <ThemeProvider theme={defaultTheme}>
+      <>
+        <Editor onChange={noop} content={null} />
+        <ResetStyle />
+      </>
+    </ThemeProvider>,
     mountPoint,
   );
 }
