40d324cff071cdbcd599a7a3d26d3d17e4078ef1
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-01 19:55:56 +0900

    feat(editor): toggle todo on click

--- src/modules/editor/EditorView.ts
+++ src/modules/editor/EditorView.ts
@@ -1,4 +1,5 @@
-import { Nullable } from 'option-t/lib/Nullable';
+import { isNull, Nullable } from 'option-t/lib/Nullable';
+import { Node } from 'prosemirror-model';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { unwrapUnsafeValue } from '../../utils/unwrapUnsafeValue';
@@ -6,6 +7,22 @@ import { EditorContent } from './editor-type';
 import { TodoNodeView } from './TodoPlugin';
 
 export type OnChange = (getContent: () => Nullable<EditorContent>) => void;
+
+function isTodo(node: Node): boolean {
+  return node.type.name === 'todo';
+}
+
+function isCheckbox(target: Nullable<EventTarget>): boolean {
+  if (isNull(target)) {
+    return false;
+  }
+
+  const element = unwrapUnsafeValue<HTMLElement>(target);
+  return (
+    element.nodeName.toLowerCase() === 'input' &&
+    element.getAttribute('type') === 'checkbox'
+  );
+}
 
 export function createEditorView(state: EditorState, onChange: OnChange) {
   const editorView = new EditorView(undefined, {
@@ -21,10 +38,25 @@ export function createEditorView(state: EditorState, onChange: OnChange) {
         return new TodoNodeView(node);
       },
     },
+    handleClickOn(view, pos, node, nodePos, event: MouseEvent) {
+      if (!isTodo(node) || !isCheckbox(event.target)) {
+        return false;
+      }
+
+      view.dispatch(
+        view.state.tr.setNodeMarkup(nodePos, undefined, {
+          checked: !node.attrs.checked,
+        }),
+      );
+
+      return true;
+    },
     state,
   });
+
   function getContent(): Nullable<EditorContent> {
     return unwrapUnsafeValue<EditorContent>(editorView.state.doc.toJSON());
   }
+
   return editorView;
 }
