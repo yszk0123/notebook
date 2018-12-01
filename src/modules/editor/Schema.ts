297e6bedef6c16c29ed5f3ff2613d31f22b02067
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-01 13:41:02 +0900

    refactor(Schema): make type definitions explicit

--- src/modules/editor/Schema.ts
+++ src/modules/editor/Schema.ts
@@ -1,16 +1,16 @@
 import { Schema } from 'prosemirror-model';
 import { schema as basicSchema } from 'prosemirror-schema-basic';
 import { addListNodes } from 'prosemirror-schema-list';
+import { unwrapUnsafeValue } from '../../utils/unwrapUnsafeValue';
+
+type OrderedMap = ReturnType<typeof addListNodes>;
 
 export function createSchema() {
+  const basicNodes = unwrapUnsafeValue<OrderedMap>(basicSchema.spec.nodes);
+  const nodes = addListNodes(basicNodes, 'paragraph block*', 'block');
+
   return new Schema({
     marks: basicSchema.spec.marks,
-    nodes: addListNodes(
-      // FIXME: Avoid any (#6)
-      // tslint:disable-next-line:no-any
-      basicSchema.spec.nodes as any,
-      'paragraph block*',
-      'block',
-    ),
+    nodes,
   });
 }
