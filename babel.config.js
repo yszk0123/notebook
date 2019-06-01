9a5b5f50c3eb7f1ac4acbc051dd733ded1f55a3c
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-06-02 03:42:17 +0900

    chore(babel): insert process.env

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-transform-modules-commonjs'],
+    plugins: [
+      '@babel/plugin-transform-modules-commonjs',
+      [
+        'babel-plugin-transform-inline-environment-variables',
+        {
+          include: [
+            'NODE_ENV',
+            'APP_BASE_URL',
+            'APP_TITLE',
+            'FIREBASE_API_KEY',
+            'FIREBASE_AUTH_DOMAIN',
+            'FIREBASE_DATABASE_URL',
+            'FIREBASE_PROJECT_ID',
+            'FIREBASE_STORAGE_BUCKET',
+            'FIREBASE_MESSAGING_SENDER_ID',
+            'FIREBASE_APP_ID',
+            'HASURA_GRAPHQL_ENDPOINT',
+            'HASURA_GRAPHQL_SUBSCRIPTION_ENDPOINT',
+          ],
+        },
+      ],
+    ],
   };
 };
