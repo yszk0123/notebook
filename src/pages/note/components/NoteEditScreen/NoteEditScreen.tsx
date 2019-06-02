dcebf05f7dde8a45691342b2fdc70911fd471359
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-06-03 00:14:01 +0900

    feat(NoteScreen): improve layout of list item

--- src/pages/note/components/NoteEditScreen/NoteEditScreen.tsx
+++ src/pages/note/components/NoteEditScreen/NoteEditScreen.tsx
@@ -1,7 +1,6 @@
 import { Button, Container, Icon, ListItem, Text, Textarea } from 'native-base';
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationScreenProp, NavigationState, ScrollView } from 'react-navigation';
+import { NavigationScreenProp, NavigationState } from 'react-navigation';
 import { defaultTheme } from '../../../../application/theme/DefaultTheme';
 import { FontSize } from '../../../../application/theme/Theme';
 import { LoadingPage } from '../../../../components/LoadingPage';
@@ -29,25 +28,23 @@ export const NoteEditScreen: React.FunctionComponent<Props> = ({ navigation }) =
 
   return (
     <Container>
-      <ScrollView style={styles.container}>
-        <ListItem>
-          <Textarea autoFocus rowSpan={6} value={text} onChangeText={onChangeText} />
-        </ListItem>
-        <ListItem>
-          <Button danger onPress={() => onDelete(noteId)}>
-            <Text>Delete</Text>
-          </Button>
-          <Button primary onPress={() => onUpdate(noteId)}>
-            <Text>Update</Text>
-          </Button>
-        </ListItem>
-      </ScrollView>
+      <ListItem>
+        <Textarea
+          autoFocus
+          placeholder="Edit"
+          rowSpan={6}
+          value={text}
+          onChangeText={onChangeText}
+        />
+      </ListItem>
+      <ListItem>
+        <Button danger onPress={() => onDelete(noteId)}>
+          <Text>Delete</Text>
+        </Button>
+        <Button primary onPress={() => onUpdate(noteId)}>
+          <Text>Update</Text>
+        </Button>
+      </ListItem>
     </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingBottom: PADDING_BOTTOM,
-  },
-});
