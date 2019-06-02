8e4fc9acdd4c409c7ac8b855ba21f2aa1aedeb39
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-06-02 13:01:52 +0900

    feat(LoadingPage): show indicator instead of text

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,12 +1,19 @@
-import { Container, Text } from 'native-base';
+import { Container } from 'native-base';
 import React from 'react';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 
 interface Props {}
 
 export const LoadingPage: React.FunctionComponent<Props> = () => {
   return (
     <Container>
-      <Text>Loading...</Text>
+      <ActivityIndicator style={styles.indicator} size="large" />
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  indicator: {
+    margin: 16,
+  },
+});
