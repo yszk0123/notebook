1b623ed34e7263f50ef0222fb1e750c131077d7c
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-06-08 10:10:03 +0900

    fix: disable workaroundForExpo on web

--- src/workaroundForExpo.ts
+++ src/workaroundForExpo.ts
@@ -2,6 +2,8 @@
  * @see https://github.com/expo/expo/issues/4070
  */
 import * as Font from 'expo-font';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
-StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
+if (Platform.OS !== 'web') {
+  StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
+}
