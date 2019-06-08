6057bf93c90cc942ab9fd0b3127be479e3807e2e
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-06-08 10:01:51 +0900

    fix(utils): fix lint errors

--- src/application/utils/debounce.ts
+++ src/application/utils/debounce.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line:no-any
 export function debounce<Args extends any[]>(fn: (...args: Args) => unknown, delay: number) {
-  let timeoutId: number;
+  let timeoutId: NodeJS.Timeout;
 
   return (...args: Args) => {
     if (timeoutId) {
