626134aec50cf3c230fafee7aec8edee1aa93883
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-06-08 10:33:47 +0900

    fix: fix login method on web

--- src/Bootstrap/components/AuthUI.tsx
+++ src/Bootstrap/components/AuthUI.tsx
@@ -2,6 +2,7 @@ import { Facebook } from 'expo';
 import firebase from 'firebase/app';
 import { Button, Container, Content, Footer, Header, Text } from 'native-base';
 import React, { useCallback } from 'react';
+import { Platform } from 'react-native';
 import { isNull, Nullable } from '../../application/utils/Maybe';
 import { FACEBOOK_APP_ID } from '../../config/AppConfig';
 
@@ -15,13 +16,18 @@ function useAuthUI(
       return;
     }
 
-    const { type, token } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
-      permissions: ['public_profile'],
-    });
+    if (Platform.OS === 'web') {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      await firebase.auth().signInWithPopup(provider);
+    } else {
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
+        permissions: ['public_profile'],
+      });
 
-    if (type === 'success') {
-      const credential = firebase.auth.FacebookAuthProvider.credential(token);
-      firebase.auth().signInWithCredential(credential);
+      if (type === 'success') {
+        const credential = firebase.auth.FacebookAuthProvider.credential(token);
+        await firebase.auth().signInWithCredential(credential);
+      }
     }
   }, [firebaseApp]);
 
